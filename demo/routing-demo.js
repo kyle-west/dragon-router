d5941ef03e13f89e23c0d56f76ab91ee544d8e14
Kyle West <kyle@familysearch.org>
2018-10-10 18:49:07 -0600

    [DerivedSubpath] added subpath derivation to the routing tokens

--- demo/routing-demo.js
+++ demo/routing-demo.js
@@ -1,4 +1,4 @@
-import { ClientRouter } from '../client-router.js';
+import { ClientRouter, RouteHandler, DerivedSubpath } from '../client-router.js';
 let router = new ClientRouter({ routerId: 'demo-router', debug: true });
 
 
@@ -13,9 +13,13 @@ const middleware2 = (context, next) => {
 };
 
 
-router.use('/demo', middleware1, middleware2, (context) => {
+let demoRouteHandler = new RouteHandler('/demo', [middleware1, middleware2, (context) => {
   document.getElementById('page-title').innerHTML = "DEMO";
-});
+}]);
+
+router.use(demoRouteHandler);
+
+
 router.use('/page', middleware1, middleware2, (context) => {
   document.getElementById('page-title').innerHTML = "Default Page";
 });
@@ -30,6 +34,32 @@ router.use(`/:section(about|contact)`, middleware1, middleware2, (context) => {
 
 router.use(`/:section(about|contact)/:subsection`, middleware1, middleware2, (context) => {
   document.getElementById('page-title').innerHTML = context.params.section + " page | " + context.params.subsection;
+});
+
+router.use(new DerivedSubpath('subsection', (context) => {
+  return Promise.resolve('default-path');
+}))
+
+router.use(`/derived-subpath/$:subsection`, middleware1, middleware2, (context) => {
+  document.getElementById('page-title').innerHTML = "DerivedSubpath: " + context.params.subsection;
+});
+
+
+router.use(new DerivedSubpath('first', (context) => {
+  return Promise.resolve('abc');
+}))
+router.use(new DerivedSubpath('second', (context) => {
+  return 'sub-sub-page';
+}))
+router.use(new DerivedSubpath('third', (context) => {
+  return 's0';
+}))
+
+router.use(`/multi-derived-subpath/$:first(abc|123)/$:second/$:third`, middleware1, middleware2, (context) => {
+  document.getElementById('page-title').innerHTML = "Muiltiple DerivedSubpath: " 
+    + context.params.first + " -> " 
+    + context.params.second + " -> " 
+    + context.params.third;
 });
 
 router.registerOn(window);
